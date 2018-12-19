2649a2c48cad1ab16208ee40d4b0f97f7537acfe
Maksat Duishenaliev <maksatovy@gmail.com>
2018-12-19 22:15:48 +0600

    Last verison for BackUp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import NewTask from "./components/NewTask.js";
 
 class App extends Component {
+  state = {
+    toDoList: [
+      { text: 'Buy milk'},
+      { text: 'Do homework'}
+      ],
+    showToDoList: true
+  };
+
+  addTask = (event, index) => {
+    const toDoListCopy = this.state.ToDoList;
+
+    toDoListCopy.text = event.target.value;
+    toDoListCopy.id = event.target.id;
+    toDoListCopy.push(NewTask);
+
+    this.setState({toDoList: toDoListCopy});
+  };
+
+  increaseAge = id => {
+    const index = this.state.toDoList.findIndex(task => task.id === id);
+
+    const toDoList = [...this.state.toDoList];
+    const task = {...this.state.toDoList[index]};
+
+
+    toDoList[index] = task;
+
+    this.setState({toDoList});
+  };
+
+  removeTask = index => {
+    const toDoListCopy = this.state.toDoList;
+    toDoListCopy.splice(index, 1);
+
+    this.setState({toDoList: toDoListCopy});
+  };
+
+  toggleTask = () => {
+    this.setState({
+      showToDoList: !this.state.showToDoList
+    });
+  };
+
   render() {
+    let toDoList = null;
+
+    if (this.state.showToDoList) {
+      toDoList = this.state.toDoList.map((task, index) => (
+        <NewTask
+          text={task}
+          id={index}
+          onClick={() => this.increaseAge(task.index)}
+          onChange={event => this.addTask(event, index)}
+          remove={() => this.removeTask(index)}>
+        </NewTask>
+      ));
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          <button onClick={this.toggleTask}>Toggle toDoList</button>
+        </div>
+        {toDoList}
       </div>
     );
   }
--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -2,19 +2,9 @@ import React from 'react';
 
 
 const NewTask = props => (
-    <div className="NewTask">
-        <h1>
-            {props.name}
-            <button onClick={props.remove}>X</button>
-        </h1>
-        <p>
-            Age: {props.age}
-            <button onClick={props.onClick}>+</button>
-        </p>
-        <p>{props.children}</p>
-        <p>
-            <input type="text" value={props.name} onChange={props.onChange} />
-        </p>
+    <div className="newTask">
+        <input type="text" value={props.text} onChange={props.onChange} />
+        <h3>{props.text}</h3>
     </div>
 );
 
